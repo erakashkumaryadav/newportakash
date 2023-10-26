ec256720e03abe246e9a4828c94444c62b77f793
Akash Coding Solutions <105480421+erakashkumaryadav@users.noreply.github.com>
2023-10-26 16:26:58 +0530

    newux

--- IMG/app.js
+++
@@ -1,6 +0,0 @@
-var type = new  Typed(".app", {
-    strings: ["App Development"],
-    typeSpeed:100,
-    backSpeed:150,
-    loop:true
-  })
--- IMG/services.js
+++
@@ -1,8 +0,0 @@
-var type = new  Typed(".services", {
-    strings: ["Let's Connect and Create Something Amazing Together"],
-    typeSpeed:50,
-    backSpeed:50,
-    loop:true
-  })
-
-  
